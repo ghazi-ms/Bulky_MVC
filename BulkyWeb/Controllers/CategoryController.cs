6faf5ee5dfcb7b225ea119b1a4e319a91144b960
Ghazi Alsalah <Ghazi.ms@hotmail.com>
2024-05-05 19:10:38 +0300

    updated coder

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Category> CategoriesList = _dbContext.Categories.ToList();
+            return View(CategoriesList);
+        }
+    }
+}
