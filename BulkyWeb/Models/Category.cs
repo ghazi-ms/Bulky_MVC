126360a3da9568d3a908d8629c6f6e94e6f48a56
Ghazi Alsalah <Ghazi.ms@hotmail.com>
2024-05-07 20:31:17 +0300

    Added Delete,Edit and toaster#

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -9,8 +9,10 @@ namespace BulkyWeb.Models
         public int Id{ get; set; }
         [Required]
         [DisplayName("Category Name")]
+        [MaxLength(31)]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage = "The Value Must Be In The Range 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
