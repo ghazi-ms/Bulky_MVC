9e7d8395c857fc29805261ca50612c02c4d43d4a
Ghazi Alsalah <Ghazi.ms@hotmail.com>
2024-05-25 22:01:05 +0300

    Lecture 81

---
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bulky.DataAccess.Data;
+
+namespace Bulky.DataAccess.Repository
+{
+	public class ProductRepository: Repository<Product>, IProductRepository
+	{
+		private ApplicationDbContext _db;
+		public ProductRepository(ApplicationDbContext db) : base(db)
+		{
+			_db = db;
+		}
+
+		public void Update(Product obj)
+		{
+			if (obj == null)
+			{
+				throw new ArgumentNullException(nameof(obj));
+			}
+
+			_db.Products.Update(obj);
+		}
+	}
+}
